df3b240da99afc214536e336f89a36db68a63caf
Erick Pac <erickj.pac@gmail.com>
2024-11-26 11:19:15 -0600

    Add helmet for security enhancements and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 import setRoutes from "./router";
 import * as middleware from "./middlewares";
 
@@ -10,6 +11,7 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
 
 setRoutes(app);
 
