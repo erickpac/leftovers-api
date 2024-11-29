191f38abd3e2d44f887573ff19f36eb6e6ba7e79
Erick Pac <erickj.pac@gmail.com>
2024-11-28 19:45:41 -0600

    Refactor user controller tests to use service module for fetching users

--- src/components/users/controller.test.ts
+++ src/components/users/controller.test.ts
@@ -1,14 +1,14 @@
 import request from "supertest";
 import express from "express";
-import * as UserController from "./controller";
-import { getAllUsers } from "@/components/users/service";
+import * as controller from "./controller";
+import * as service from "@/components/users/service";
 import { sendSuccessResponse, sendErrorResponse } from "@/common/responses";
 
 jest.mock("@/components/users/service");
 jest.mock("@/common/responses");
 
 const app = express();
-app.get("/users", UserController.getUsers);
+app.get("/users", controller.getUsers);
 
 describe("UserController", () => {
   describe("GET /users", () => {
@@ -20,7 +20,7 @@ describe("UserController", () => {
     });
 
     it("should return a list of users", async () => {
-      (getAllUsers as jest.Mock).mockResolvedValue(mockUsers);
+      (service.getAllUsers as jest.Mock).mockResolvedValue(mockUsers);
       (sendSuccessResponse as jest.Mock).mockImplementation(({ res, data }) => {
         res.status(200).json(data);
       });
@@ -29,7 +29,7 @@ describe("UserController", () => {
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual(mockUsers);
-      expect(getAllUsers).toHaveBeenCalledTimes(1);
+      expect(service.getAllUsers).toHaveBeenCalledTimes(1);
       expect(sendSuccessResponse).toHaveBeenCalledWith({
         res: expect.anything(),
         data: mockUsers,
@@ -37,7 +37,9 @@ describe("UserController", () => {
     });
 
     it("should handle errors", async () => {
-      (getAllUsers as jest.Mock).mockRejectedValue(new Error(errorMessage));
+      (service.getAllUsers as jest.Mock).mockRejectedValue(
+        new Error(errorMessage),
+      );
       (sendErrorResponse as jest.Mock).mockImplementation(
         ({ res, message }) => {
           res.status(500).json({ error: message });
@@ -48,7 +50,7 @@ describe("UserController", () => {
 
       expect(response.status).toBe(500);
       expect(response.body).toEqual({ error: errorMessage });
-      expect(getAllUsers).toHaveBeenCalledTimes(1);
+      expect(service.getAllUsers).toHaveBeenCalledTimes(1);
       expect(sendErrorResponse).toHaveBeenCalledWith({
         res: expect.anything(),
         message: errorMessage,
