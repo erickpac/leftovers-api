53f20e10312db560cf3d548d14460f3683a2b3fa
Erick Pac <erickj.pac@gmail.com>
2024-12-02 12:25:03 -0600

    Add JSDoc comments for notFound middleware in middlewares.ts

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -4,13 +4,17 @@ import { sendErrorResponse } from "@/common/responses";
 /**
  * Middleware to handle requests to routes that are not found.
  *
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next middleware function in the stack.
+ *
+ * @remarks
  * This middleware creates an error with a message indicating the requested URL
  * was not found and sets the response status to 404. It then passes the error
  * to the next middleware in the stack.
  *
- * @param req - The request object.
- * @param res - The response object.
- * @param next - The next middleware function in the stack.
+ * @example
+ * app.use(notFound);
  */
 export const notFound = (req: Request, res: Response, next: NextFunction) => {
   const error = new Error(`🔍 - not found - ${req.originalUrl}`);
