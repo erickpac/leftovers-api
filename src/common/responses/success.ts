c3b08683a64b05d9c3901bf7f020b85287ed1903
Erick Pac <erickj.pac@gmail.com>
2024-12-01 23:57:19 -0600

    Add documentation for success response function

--- src/common/responses/success.ts
+++ src/common/responses/success.ts
@@ -1,6 +1,15 @@
 import SuccessResponse from "@/types/success-response";
 import type { Response } from "express";
 
+/**
+ * Sends a success response with the specified status code, info, and data.
+ *
+ * @param res - The response object.
+ * @param status - The HTTP status code for the success response (default is 200).
+ * @param info - The optional info message to be included in the response.
+ * @param data - The data to be included in the response.
+ * @returns The response object with the success details.
+ */
 const success = ({
   res,
   status = 200,
