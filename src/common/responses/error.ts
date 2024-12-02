dc9329142ceedc6d6f5b36edf372953472c16920
Erick Pac <erickj.pac@gmail.com>
2024-12-01 23:54:25 -0600

    Add documentation for error response function

--- src/common/responses/error.ts
+++ src/common/responses/error.ts
@@ -1,6 +1,15 @@
 import ErrorResponse from "@/types/error-response";
 import type { Response } from "express";
 
+/**
+ * Sends an error response with the specified status code, message, and optional stack trace.
+ *
+ * @param res - The response object.
+ * @param statusCode - The HTTP status code for the error response (default is 500).
+ * @param message - The error message to be included in the response.
+ * @param stack - The optional stack trace to be included in the response.
+ * @returns The response object with the error details.
+ */
 const error = ({
   res,
   statusCode = 500,
