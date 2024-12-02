626f887b41c76f839ea101cd27d90e3f42e52008
Erick Pac <erickj.pac@gmail.com>
2024-12-01 23:59:08 -0600

    Refactor pagination utility functions to use const and add documentation for calculatePagination

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,15 +1,15 @@
-export const getCurrentPageNumber = (page: string): number => {
+const getCurrentPageNumber = (page: string): number => {
   return page ? Number(page) : 1;
 };
 
-export const getTotalPages = (
+const getTotalPages = (
   totalItems: number,
   itemsPerPage: number = 20,
 ): number => {
   return Math.ceil(totalItems / itemsPerPage);
 };
 
-export const getNextPageUrl = (
+const getNextPageUrl = (
   currentPage: number,
   totalPages: number,
   endpoint: string = "stores",
@@ -23,7 +23,7 @@ export const getNextPageUrl = (
   }/${endpoint}?page=${currentPage + 1}` as string;
 };
 
-export const getPreviousPageUrl = (
+const getPreviousPageUrl = (
   currentPage: number,
   totalPages: number,
   endpoint: string = "stores",
@@ -37,6 +37,18 @@ export const getPreviousPageUrl = (
   }/${endpoint}?page=${currentPage - 1}` as string;
 };
 
+/**
+ * Calculates pagination details based on the provided query page, total items, and endpoint.
+ *
+ * @param queryPage - The current page number as a string from the query.
+ * @param totalItems - The total number of items available.
+ * @param endpoint - The API endpoint for generating pagination URLs (default is "stores").
+ * @returns An object containing pagination details including total items, current page, total pages,
+ *          next page URL, and previous page URL if successful. If an error occurs, returns an object
+ *          with success set to false and an error message.
+ *
+ * @throws Will throw an error if no items are found or if the current page number is invalid.
+ */
 export const calculatePagination = (
   queryPage: string,
   totalItems: number,
