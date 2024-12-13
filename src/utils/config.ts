a94565c18251b65b965abc4daccdda3811527450
Erick Pac <erickj.pac@gmail.com>
2024-12-13 11:30:04 -0600

    Validate environment variables to ensure they are not empty

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 const envSchema = z.object({
   PORT: z.string().default("3000"),
-  DATABASE_URL: z.string(),
-  CURRENT_URL: z.string(),
-  API_VERSION: z.string(),
+  DATABASE_URL: z.string().min(1, "Database URL must be not empty"),
+  CURRENT_URL: z.string().min(1, "Current URL must be not empty"),
+  API_VERSION: z.string().min(1, "API version must be not empty"),
 });
 
 const { success, error, data } = envSchema.safeParse(process.env);
